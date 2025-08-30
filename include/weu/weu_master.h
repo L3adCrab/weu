a8818f382043ebe963a3d7002bfb9be95ab6636e
L3adCrab <l3adcrab@gmail.com>
2025-08-30 10:43:39 +0300

    feat: master header file

---
+++ include/weu/weu_master.h
@@ -0,0 +1,27 @@
+#ifndef weu_master_h
+#define weu_master_h
+
+/*///////////////////////////////////////////////////////////////////////////////////
+//  USAGE
+//  By default functions are defined as extern.
+//  To implement somewhere in source file before including header file
+//  #define WEU_IMPLEMENTATION
+//  Implementation should be defined only once.
+//  
+//  For use as static functions before including header file
+//  #define WEU_STATIC
+//  There if no need to define WEU_IMPLEMENTATION when using as static,
+//  although WEU_STATIC will need to be somewhere defined in every source file where
+//  library will be used. To circumvent this and whole library will be used as static
+//  add the WEU_STATIC define to compiler (gcc/clang - -DWEU_STATIC)
+//
+//  To include all weu library in souce file at once, include weu_master.h
+*////////////////////////////////////////////////////////////////////////////////////
+
+#include "weu_bitfield.h"
+#include "weu_hashtable.h"
+#include "weu_list.h"
+#include "weu_pair.h"
+#include "weu_string.h"
+
+#endif
