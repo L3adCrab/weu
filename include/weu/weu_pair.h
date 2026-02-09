fde5137e0eb36a7682ce8071a5843bb0fe733f58
L3adCrab <l3adcrab@gmail.com>
2026-02-09 19:35:05 +0200

    feat datapair implemented to pair

--- include/weu/weu_pair.h
+++ include/weu/weu_pair.h
@@ -1,7 +1,8 @@
 /*///////////////////////////////////////////////////////////////////////////////////
 //  SPDX-License-Identifier: Unlicense
 /////////////////////////////////////////////////////////////////////////////////////
-//  USAGE
+//  IMPLEMENTATION
+//
 //  Functions are defined as extern.
 //  To implement somewhere in source file before including header file
 //  #define WEU_IMPLEMENTATION
@@ -11,97 +12,138 @@
 //  #include <path_to_lib/weu_master.h>
 //
 //  To include all weu library in souce file at once, include weu_master.h
+/////////////////////////////////////////////////////////////////////////////////////
+// EXAMPLE Create dataPair and set and getting data
+#include <stdio.h>
+
+#define WEU_IMPLEMENTATION
+#include "include/weu/weu_datapair.h"
+#include "include/weu/weu_string.h"
+
+int main() {
+    // allocate data
+    uint32_t        data1 = 47;
+    weu_string      *data2 = weu_string_new("test string");
+    printf("DATA TO BE SAVED\n");
+    printf("int - %i | str - %s\n", data1, data2->text);
+    
+    //  allocate pair and set data
+    weu_pair    *pair = weu_pair_new(sizeof(data1), sizeof(data2));
+    weu_pair_setData(pair, &data1, &data2);
+
+    //  get data
+    uint32_t        out1;
+    weu_string      *out2;
+    weu_pair_getData(pair, &out1, &out2);
+    printf("DATA RETRIEVED FROM PAIR\n");
+    printf("int - %i | str - %s\n", out1, out2->text);
+
+    //  free string and pair
+    weu_string_free(&out2);
+    weu_pair_free(&pair);
+}
 *////////////////////////////////////////////////////////////////////////////////////
 
-#ifndef weu_pair_h
-#define weu_pair_h
+#ifndef WEU_PAIR_H
+#define WEU_PAIR_H
 
 #define WEUDEF extern
 
 #include "weu_datatypes.h"
 
-#include <stdlib.h>
-
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 //  ALLOCATION
 
-WEUDEF weu_pair weu_pair_new(void *v1, void *v2, datafreefun d1, datafreefun d2);
-WEUDEF void weu_pair_freeData(weu_pair *h);
-
-WEUDEF weu_pair *weu_pair_newAlloc(void *v1, void *v2, datafreefun d1, datafreefun d2);
+WEUDEF weu_pair *weu_pair_new(uint32_t dataSize_1, uint32_t dataSize_2, datafreefun destructor_1, datafreefun destructor_2);
 WEUDEF void weu_pair_free(weu_pair **h);
+
+WEUDEF void weu_pair_setDestructor1(weu_pair *h, datafreefun destructor_1);
+WEUDEF void weu_pair_setDestructor2(weu_pair *h, datafreefun destructor_2);
 /////////////////////////////////////////////////////////////////////////////////////////////////////
-//  DATA
+//  GET DATA
 
-WEUDEF void *weu_pair_getV1(weu_pair *h);
-WEUDEF void *weu_pair_getV2(weu_pair *h);
-
-WEUDEF void weu_pair_setV1(weu_pair *h, void* v, bool freePrevious);
-WEUDEF void weu_pair_setV2(weu_pair *h, void* v, bool freePrevious);
+WEUDEF void weu_pair_getData(weu_pair *h, void *out1, void *out2);
+WEUDEF void weu_pair_getData1(weu_pair *h, void *out);
+WEUDEF void weu_pair_getData2(weu_pair *h, void *out);
 /////////////////////////////////////////////////////////////////////////////////////////////////////
-//  DATA DESTRUCTOR
+//  SET DATA
 
-WEUDEF void weu_pair_setFreeFun(weu_pair *h, datafreefun d1, datafreefun d2);
+WEUDEF void weu_pair_setData(weu_pair *h, void *data1, void *data2);
+WEUDEF void weu_pair_setData1(weu_pair *h, void *data);
+WEUDEF void weu_pair_setData2(weu_pair *h, void *data);
 
 #ifdef WEU_IMPLEMENTATION
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <memory.h>
+
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 //  ALLOCATION
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 
-weu_pair weu_pair_new(void *v1, void *v2, datafreefun d1, datafreefun d2) {
-    weu_pair out = {v1, v2, d1, d2};
-    return out;
-}
-void weu_pair_freeData(weu_pair *h) {
-    if (h == NULL) return;
-    if (h->d1 != NULL) h->d1(&h->v1);
-    if (h->d2 != NULL) h->d2(&h->v2);
-}
-
-weu_pair *weu_pair_newAlloc(void *v1, void *v2, datafreefun d1, datafreefun d2) {
+weu_pair *weu_pair_new(uint32_t dataSize_1, uint32_t dataSize_2, datafreefun destructor_1, datafreefun destructor_2) {
     weu_pair *out = (weu_pair*)malloc(sizeof(weu_pair));
-    out->v1 = v1;
-    out->v2 = v2;
-    out->d1 = d1;
-    out->d2 = d2;
+    out->dataSize1 = dataSize_1;
+    out->dataSize2 = dataSize_2;
+    out->data = calloc(1, dataSize_1 + dataSize_2);
+    out->d1 = destructor_1;
+    out->d2 = destructor_2;
     return out;
 }
 void weu_pair_free(weu_pair **h) {
     if (*h == NULL) return;
     weu_pair *pair = *h;
-    if (pair->d1 != NULL) pair->d1(&pair->v1);
-    if (pair->d2 != NULL) pair->d2(&pair->v2);
+    if (pair->d1)   pair->d1(pair->data);
+    if (pair->d2)   pair->d2(pair->data + pair->dataSize1);
+    free((*h)->data);
     free(*h);
     *h = NULL;
 }
+
+void weu_pair_setDestructor1(weu_pair *h, datafreefun destructor_1) {
+    if (!h) return;
+    h->d1 = destructor_1;
+}
+void weu_pair_setDestructor2(weu_pair *h, datafreefun destructor_2) {
+    if (!h) return;
+    h->d2 = destructor_2;
+}
 /////////////////////////////////////////////////////////////////////////////////////////////////////
-//  DATA
+//  GET DATA
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 
-void *weu_pair_getV1(weu_pair *h) {
-    return h->v1;
+void weu_pair_getData(weu_pair *h, void *out1, void *out2) {
+    if (h == NULL) { printf("pair = null\n"); out1 = NULL; out2 = NULL; return; }
+    memmove(out1, h->data, h->dataSize1);
+    memmove(out2, h->data + h->dataSize1, h->dataSize2);
 }
-void *weu_pair_getV2(weu_pair *h) {
-    return h->v2;
+void weu_pair_getData1(weu_pair *h, void *out) {
+    if (h == NULL) { out = NULL; return; }
+    memmove(out, h->data, h->dataSize1);
 }
-
-void weu_pair_setV1(weu_pair *h, void* v, bool freePrevious) {
-    if (freePrevious && h->d1 != NULL) h->d1(&h->v1);
-    h->v1 = v;
-}
-void weu_pair_setV2(weu_pair *h, void* v, bool freePrevious) {
-    if (freePrevious && h->d2 != NULL) h->d2(&h->v2);
-    h->v2 = v;
+void weu_pair_getData2(weu_pair *h, void *out) {
+    if (h == NULL) { out = NULL; return; }
+    memmove(out, h->data + h->dataSize1, h->dataSize2);
 }
 /////////////////////////////////////////////////////////////////////////////////////////////////////
-//  DATA DESTRUCTOR
+//  SET DATA
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 
-void weu_pair_setFreeFun(weu_pair *h, datafreefun d1, datafreefun d2) {
+void weu_pair_setData(weu_pair *h, void *data1, void *data2) {
     if (h == NULL) return;
-    h->d1 = d1;
-    h->d2 = d2;
+    weu_pair_setData1(h, data1);
+    weu_pair_setData2(h, data2);
+}
+void weu_pair_setData1(weu_pair *h, void *data) {
+    if (h == NULL) return;
+    if (data == NULL)   memset(h->data, 0, h->dataSize1);
+    else                memmove(h->data, data, h->dataSize1);
+}
+void weu_pair_setData2(weu_pair *h, void *data) {
+    if (h == NULL) return;
+    if (data == NULL)   memset(h->data + h->dataSize1, 0, h->dataSize2);
+    else                memmove(h->data + h->dataSize1, data, h->dataSize2);
 }
 
 #endif
